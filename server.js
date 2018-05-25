01c969b164749ebe369b245dcf596d542bfb4384
Max Berta <mberta@apple.com>
2018-05-25 10:59:21 +0100

    testing env for Heroku

--- server.js
+++ server.js
@@ -1,1 +1,13 @@
-console.log("Starting app...")
+const express = require('express')
+const app = express()
+
+if(process.env.heroku){
+  app.get('/', (req, res) => res.send('Running on Heroku'))
+} else {
+  require('dotenv').load();
+  var dburl=process.env.dburl
+  app.get('/', (req, res) => res.send('Not running on Heroku, read env file dburl is:'+dburl))
+}
+
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
